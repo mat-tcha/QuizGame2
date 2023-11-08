05c336649f13b8694d4ae75fa341f57fe80d8554
MatsumotoShouma <135771927+mat-tcha@users.noreply.github.com>
2023-11-08 18:06:17 +0900

    Initial commit

---
+++ Assets/TextMesh Pro/SceneController6.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;　　　//ボタンを使用するのでUI
+using UnityEngine.SceneManagement;//SceneManagerを使用ためSceneManagementを追加
+
+public class SceneController6 : MonoBehaviour
+{
+
+    // ボタンをクリックするとScene『B』に移動
+    public void ButtonClick()
+    {
+        SceneManager.LoadScene("結果発表5");
+    }
+}
